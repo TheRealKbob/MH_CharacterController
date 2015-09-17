e05da753b194d1901e4177bbacbe48e2deb43482
Anthony Capobianchi <actionkbob@gmail.com>
2015-09-16 22:56:22 -0400

    Third person controller pivot, animation nodes, third person camera follow

---
+++ Assets/Scripts/Model/Camera/PlayerCamera.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	[SerializeField]
+	private float distance = 5; //In Meters
+
+	[SerializeField]
+	private float angle = 2;
+
+	[SerializeField]
+	private float smooth = 3;
+
+	[SerializeField]
+	private Vector3 offset = new Vector3(0, 0, 0);
+
+	[SerializeField]
+	private float targetDampTime = 0.1f;
+
+	private Vector3 velocityTargetSmooth = Vector3.zero;
+	private Vector3 lookDirection;
+	private Transform target;
+	private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag( "CameraAnchor" ).transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		Vector3 targetOffset = target.position + offset;
+
+		lookDirection = targetOffset - this.transform.position;
+		lookDirection.y = 0f;
+		lookDirection.Normalize();
+
+		targetPosition = targetOffset + Vector3.up * angle - lookDirection * distance;
+		smoothPosition( this.transform.position, targetPosition );
+
+		transform.LookAt( target );
+	}
+
+	private void smoothPosition( Vector3 fromPosition, Vector3 toPosition )
+	{
+		transform.position = Vector3.SmoothDamp( fromPosition, toPosition, ref velocityTargetSmooth, targetDampTime );
+	}
+
+}
--- Assets/Scripts/Model/Player/CharacterLocomotion.cs
+++ Assets/Scripts/Model/Player/CharacterLocomotion.cs
@@ -3,12 +3,42 @@ using System.Collections;
 
 public class CharacterLocomotion : MonoBehaviour {
 
+	[SerializeField]
+	private Animator animator;
+	private AnimatorStateInfo stateInfo;
+
+	[SerializeField]
+	private GameObject pivotObject;
+
+	[SerializeField]
+	private float directionDampTime = 0.25f;
+
+	[SerializeField]
+	private float directionSpeed = 3.0f;
+
+	[SerializeField]
+	private float rotationDegreePerSecond = 120f;
+
+	private float speed;
+	private float direction;
+
+
 	private InputBindings bindings;
 	private CharacterActions actions;
 
+	private int m_LocomotionID = 0;
+
+	public bool isInLocomotion{ get{ return stateInfo.nameHash == m_LocomotionID; } }
+
+
 	// Use this for initialization
 	void Start () {
-		
+
+		if(animator == null)
+			animator = GetComponent<Animator>() as Animator;
+
+		m_LocomotionID = Animator.StringToHash( "Base Layer.Locomotion" );
+
 	}
 
 	public void Init( InputBindings bindings )
@@ -18,9 +48,11 @@ public class CharacterLocomotion : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if(bindings != null)
 		{
+			stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
 			if( actions.Move.IsPressed )
 			{
 				ApplyMoveValue( actions.Move.Value );
@@ -30,6 +62,41 @@ public class CharacterLocomotion : MonoBehaviour {
 
 	public void ApplyMoveValue( Vector2 force )
 	{
+		InputToWorldSpace( force, this.transform, pivotObject.transform, ref direction, ref speed );
+		animator.SetFloat( "Speed", speed );
+		animator.SetFloat( "Direction", direction, directionDampTime, Time.deltaTime );
+
+		if( isInLocomotion && ( ( direction >= 0 && force.x >= 0 ) || ( direction < 0 && force.x < 0 ) ) )
+		{
+			Vector3 newRotationY = new Vector3( 0f, rotationDegreePerSecond * ( force.x < 0f ? -1f : 1f), 0f );
+			Vector3 rotationAmount = Vector3.Lerp( Vector3.zero, newRotationY, Mathf.Abs(force.x ) );
+			Quaternion deltaRotation = Quaternion.Euler( rotationAmount * Time.deltaTime );
+			this.transform.rotation = ( this.transform.rotation * deltaRotation );
+		}
 
 	}
+
+	private void InputToWorldSpace( Vector2 input, Transform root, Transform pivot, ref float directionOut, ref float speedOut )
+	{
+		Vector3 rootDirection = root.forward;
+		Vector3 pivotDirection = pivot.forward;
+		pivotDirection.y = 0f;
+
+		Vector3 inputDirection = new Vector3( input.x, 0, input.y );
+		speedOut = inputDirection.sqrMagnitude;
+
+		Quaternion referentialShift = Quaternion.FromToRotation( Vector3.forward, pivotDirection );
+
+		Vector3 moveDirection = referentialShift * inputDirection;
+		Vector3 axisSign = Vector3.Cross( moveDirection, rootDirection );
+
+		float angleRootToMove = Vector3.Angle( rootDirection, moveDirection ) * (axisSign.y >= 0 ? -1f : 1f);
+
+		angleRootToMove /= 180f;
+
+		directionOut = angleRootToMove * directionSpeed;
+
+	}
+
+
 }
